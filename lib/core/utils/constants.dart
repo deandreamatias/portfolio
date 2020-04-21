6f02201eb7ad1361bc0a795c5cbfde30f441e84f
Matias de Andrea <deandreamatias@gmail.com>
2020-04-21 16:32:37 +0200

    Fix images in Safari broswers

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,19 +1,18 @@
 class Assets {
-  static const String ADOBEXD = 'assets/img/adobe-xd.webp';
-  static const String ANDROID = 'assets/img/android.webp';
-  static const String FLUTTER = 'assets/img/flutter.webp';
+  static const String ADOBEXD = 'assets/img/adobe-xd.png';
+  static const String ANDROID = 'assets/img/android.png';
+  static const String FLUTTER = 'assets/img/flutter.png';
   static const String GIT = 'assets/img/git.png';
-  static const String PROFILE = 'assets/img/profile.webp';
-  static const String SPOTIFY = 'assets/img/spotify.webp';
-  static const String GOOGLE_PODCASTS = 'assets/img/google_podcasts.webp';
-  static const String ANCHOR = 'assets/img/anchor.webp';
-  static const String APPLE_PODCAST = 'assets/img/apple_podcasts.webp';
-  static const String DEEZER = 'assets/img/deezer.webp';
-  static const String POCKET_CASTS = 'assets/img/pocket_casts.webp';
-  static const String CASTBOX = 'assets/img/castbox.webp';
-  static const String RADIOPUBLIC = 'assets/img/radiopublic.webp';
-  static const String OVERCAST = 'assets/img/overcast.webp';
-  static const String BREAKER = 'assets/img/breaker.webp';
+  static const String SPOTIFY = 'assets/img/spotify.png';
+  static const String GOOGLE_PODCASTS = 'assets/img/google_podcasts.png';
+  static const String ANCHOR = 'assets/img/anchor.png';
+  static const String APPLE_PODCAST = 'assets/img/apple_podcasts.png';
+  static const String DEEZER = 'assets/img/deezer.png';
+  static const String POCKET_CASTS = 'assets/img/pocket_casts.png';
+  static const String CASTBOX = 'assets/img/castbox.png';
+  static const String RADIOPUBLIC = 'assets/img/radiopublic.png';
+  static const String OVERCAST = 'assets/img/overcast.png';
+  static const String BREAKER = 'assets/img/breaker.png';
 }
 
 class Urls {
