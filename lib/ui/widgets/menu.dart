ae4012e37c82e314c0ffae422cb6908b7a67462f
Matias de Andrea <deandreamatias@gmail.com>
2021-03-18 23:09:11 +0100

    feat: Remove options menu

--- lib/ui/widgets/menu.dart
+++ lib/ui/widgets/menu.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:portfolio/core/utils/constants.dart';
-import 'package:portfolio/core/utils/navigate_links.dart';
 
-import '../shared/portfolio_icons.dart';
+import '../../core/utils/constants.dart';
+import '../../core/utils/navigate_links.dart';
 import '../views/home.dart';
 
 enum MenuItems { en, es, pt }
@@ -31,76 +30,6 @@ class MenuWidget extends StatelessWidget {
               style: const TextStyle(fontFamily: 'Sniglet'),
             ),
           ),
-        ),
-        const SizedBox(width: 8.0, height: 8.0),
-        PopupMenuButton<MenuItems>(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          icon: const Icon(CustomIcons.options),
-          tooltip: AppLocalizations.of(context)!.menuOptions,
-          onSelected: (MenuItems value) {
-            switch (value) {
-              case MenuItems.es:
-                AppLocalizations.delegate.load(const Locale('es', ''));
-                break;
-              case MenuItems.pt:
-                AppLocalizations.delegate.load(const Locale('pt', ''));
-                break;
-              default:
-                AppLocalizations.delegate.load(const Locale('en', ''));
-            }
-          },
-          itemBuilder: (BuildContext context) => <PopupMenuItem<MenuItems>>[
-            PopupMenuItem<MenuItems>(
-              value: MenuItems.en,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Visibility(
-                      visible: AppLocalizations.of(context)!.localeName == 'en',
-                      child: const Icon(CustomIcons.checkmark),
-                    ),
-                    Text(AppLocalizations.of(context)!.menuOptionsEnglish),
-                  ],
-                ),
-              ),
-            ),
-            PopupMenuItem<MenuItems>(
-              value: MenuItems.es,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Visibility(
-                      visible: AppLocalizations.of(context)!.localeName == 'es',
-                      child: const Icon(CustomIcons.checkmark),
-                    ),
-                    Text(AppLocalizations.of(context)!.menuOptionsSpanish),
-                  ],
-                ),
-              ),
-            ),
-            PopupMenuItem<MenuItems>(
-              value: MenuItems.pt,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Visibility(
-                      visible: AppLocalizations.of(context)!.localeName == 'pt',
-                      child: const Icon(CustomIcons.checkmark),
-                    ),
-                    Text(AppLocalizations.of(context)!.menuOptionsPortuguese),
-                  ],
-                ),
-              ),
-            ),
-          ],
         ),
       ],
     );
