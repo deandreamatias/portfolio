42c31f8d2aa9419d43dd1777041a30aa693c0f8f
Matias de Andrea <deandreamatias@gmail.com>
2024-07-27 13:32:50 +0200

    style: format file

--- lib/ui/shared/util.dart
+++ lib/ui/shared/util.dart
@@ -4,7 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 TextTheme createTextTheme(
     BuildContext context, String bodyFontString, String displayFontString) {
   TextTheme baseTextTheme = Theme.of(context).textTheme;
-  TextTheme bodyTextTheme = GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
+  TextTheme bodyTextTheme =
+      GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
   TextTheme displayTextTheme =
       GoogleFonts.getTextTheme(displayFontString, baseTextTheme);
   TextTheme textTheme = displayTextTheme.copyWith(
