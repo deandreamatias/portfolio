7d605ff26ecdb1bb1eeaca1e44068bf19e42548e
Matias de Andrea <deandreamatias@gmail.com>
2024-07-28 20:55:54 +0200

    feat: remove image assets

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,10 +1,3 @@
-class Assets {
-  static const String adobXd = 'assets/img/adobe_xd.png';
-  static const String android = 'assets/img/android.png';
-  static const String flutter = 'assets/img/flutter.png';
-  static const String git = 'assets/img/git.png';
-}
-
 class Urls {
   static const String podcast =
       'https://podcasters.spotify.com/pod/show/universoflutter';
